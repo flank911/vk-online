f1c8811d245469d1be6747e224e6f0f7a52b0e4f
root <root@159849.simplecloud.ru>
2017-10-24 19:03:58 +0000

    update

--- app/Console/Commands/online.php
+++ app/Console/Commands/online.php
@@ -37,6 +37,11 @@ class online extends Command
      */
     public function handle()
     {
-     $this->info('Тестовая строчка');   //
+     $this->info('Тестовая строчка'); 
+$access_token = "6e275627f68a72b77781d3da2dc49b9c188358b28cd9ba258564337689b12fec8652b03650c2187a4cf84";
+    $v = "5.52";
+    $user_id = 11674338;
+    $fields = "last_seen";
+    $url = "https://api.vk.com/method/users.get?"."access_token=".$access_token."&user_id=".$user_id."&fields=".$fields."&v=".$v;  //
     }
 }
